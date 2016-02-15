ece3823b4351c56d6c514b9f278117bf10d5e171
Marco Heumann (mheumann) <marco.heumann@web.de>
2016-02-15 11:43:28 +0100

    AES encryption nearly without working mixColumn

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: Marco Heumann
+ *
+ * Created on 29. Januar 2016, 20:43
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    encrypt();
+    return (EXIT_SUCCESS);
+}
+
