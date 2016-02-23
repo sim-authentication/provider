2a5e69b12bff4f596c7d98356d2b0e6e79c6e506
Marco Heumann (mheumann) <marco.heumann@web.de>
2016-02-24 00:06:31 +0100

    final AES encryption

--- main.c
+++ main.c
@@ -7,13 +7,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+typedef u_int8_t u8;
+
+u8 inputArr[16], keyArr[16], outputArr[16];
+u8 test[4][4];
+int i;
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    encrypt();
+    char* keyStr = "2b7e151628aed2a6abf7158809cf4f3c";
+    char* inputStr = "6bc1bee22e409f96e93d7e117393172a";
+    char* temp;
+    temp = malloc(3);
+    for (i = 0; i < 16; i++) {
+        strncpy(temp, &keyStr[i * 2], 2);
+        keyArr[i] = strtol(temp, NULL, 16);
+
+        strncpy(temp, &inputStr[i * 2], 2);
+        inputArr[i] = strtol(temp, NULL, 16);
+    }
+    free(temp);
+
+    encrypt(inputArr, keyArr, outputArr);
     return (EXIT_SUCCESS);
 }
 
